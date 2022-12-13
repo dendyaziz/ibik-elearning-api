e7518c2838ff120ee52a3c5fb5def8bd75427027
Dendy Armandiaz Aziz <dendy.aziz@gmail.com>
2022-12-13 07:01:39 +0700

    first commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        return Student::get();
+    }
+
+    public function store(Request $request)
+    {
+        $validated = Validator::make($request->all(), [
+            'student_number' => ['required', 'min:4', 'unique:students'],
+            'email' => ['required', 'email'],
+            'name' => ['required'],
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json($validated->errors(), 422);
+        }
+
+        $student = Student::create([
+            'student_number' => $request->student_number,
+            'email' => $request->email,
+            'name' => $request->name,
+        ]);
+
+        $student = Student::create([
+            'student_number' => $request->student_number,
+            'email' => $request->email,
+            'name' => $request->name,
+        ]);
+
+        return [
+            'data' => $student,
+            'message' => 'Mahasiswa berhasil didaftarkan.'
+        ];
+    }
+
+    public function show($id)
+    {
+        // otomatis search by kolom id, dan hanya ambil data pertama
+        $student = Student::find($id);
+
+        $student = Student::where('id', $id)->first();
+
+        return [
+            'data' => $student
+        ];
+    }
+
+    public function update(Request $request, $studentNumber)
+    {
+        $validated = Validator::make($request->all(), [
+            'email' => ['required', 'email'],
+            'name' => ['required'],
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json($validated->errors(), 422);
+        }
+
+        $student = Student::where('student_number', $studentNumber)->first();
+
+        if (!$student) {
+            return response()->json([
+                'message' => 'Mahasiswa tidak ditemukan'
+            ], 404);
+        }
+
+        // Menggunakan ->only agar spesifik field apa saja yang diinput
+        $student = $student->update($request->only([
+            'name',
+            'email'
+        ]));
+
+        return [
+            'data' => $student,
+            'message' => 'Data Mahasiswa berhasil diperbaharui.'
+        ];
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+
+        if (!$student) {
+            return response()->json([
+                'message' => 'Mahasiswa tidak ditemukan'
+            ], 404);
+        }
+
+        $student->delete();
+
+        return [
+            'message' => 'Data Mahasiswa berhasil dihapus.'
+        ];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// ambil semua data mahasiswa
+Route::get('/students', [StudentController::class, 'index']);
+
+// ambil semua data mahasiswa
+Route::get('/students/{id}', [StudentController::class, 'show']);
+
+// buat data mahasiswa
+Route::post('/students', [StudentController::class, 'store']);
+
+// ubah data mahasiswa
+Route::put('/students/{student_number}', [StudentController::class, 'update']);
+
+// hapus data mahasiswa
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
